f41fdeaabd72d2a8fc0f958a09dc3840b68444a9
root <seholevas@gmail.com>
2020-07-09 18:26:20 -0400

    t

--- src/react/modules/grid.js
+++ src/react/modules/grid.js
@@ -7,21 +7,21 @@ import "../../css/node.css"
 // import { grabItem, releaseItem, holdingItem } from '../../helpers/action-listeners/on-mouse';
 
 export default function Grid() {
-// return <div></div>
-    const grid = useSelector(state => state.grid)
+return <div></div>
+    // const grid = useSelector(state => state.grid)
     
-    return <div className="grid visualizer">
-    {grid.map((value, row_index) =>
-    <div key={`row-${row_index}`}className={`row row-${row_index}`}>
-            {grid.map((val, column_index) => 
+    // return <div className="grid visualizer">
+    // {grid.map((value, row_index) =>
+    // <div key={`row-${row_index}`}className={`row row-${row_index}`}>
+    //         {grid.map((val, column_index) => 
 
-            <MasterNode key={`${row_index}-${column_index}`} id={`${row_index}-${column_index}`} type={value[column_index]['type']} className={`flex-item visited-${value[column_index]['visited']} shortest-path-${value[column_index]['shortest_path']}`} onClick={()=>{nodeClicked(`${row_index}-${column_index}`)}}>
+    //         <MasterNode key={`${row_index}-${column_index}`} id={`${row_index}-${column_index}`} type={value[column_index]['type']} className={`flex-item visited-${value[column_index]['visited']} shortest-path-${value[column_index]['shortest_path']}`} onClick={()=>{nodeClicked(`${row_index}-${column_index}`)}}>
                 
-                </MasterNode>
-            )}
-            </div>
-    )}
+    //             </MasterNode>
+    //         )}
+    //         </div>
+    // )}
     
-    </div> 
+    // </div> 
 
 }
