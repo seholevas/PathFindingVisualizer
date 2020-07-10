4468ea968f72b84780e77088cc5925adda086bea
root <seholevas@gmail.com>
2020-07-09 23:00:33 -0400

    added: even more async functions

--- src/helpers/state-functions/clear-matrix.js
+++ src/helpers/state-functions/clear-matrix.js
@@ -3,82 +3,6 @@ import { dispatchedChangedNodeType } from "../../redux/dispatchs/grid-dispatchs"
 import store from "../../redux/stores/store";
 import { dispatchedRemovedAdditionalDestination, dispatchedRemovedWeight, dispatchedRemovedWall, dispatchedMovedStart, dispatchedMovedEnd } from "../../redux/dispatchs/node-dispatchs";
 
-// export function clearMatrix(isClearAll = true) 
-// {
-//     // const size_of_matrix = getValueByElementId("size");
-//     const grid_size = store.getState().grid.length;
-//     const matrix = generateMatrix(grid_size);
-//     const shallow_copy = shallowCopy(matrix);
-//     const start = store.getState().nodes.start_coordinates;
-//     const end = store.getState().nodes.end_coordinates;
-
-
-//     if ((start[0] < grid_size - 1) && (start[1] < grid_size - 1)) {
-//         shallow_copy[start[0]][start[1]] = { type: "start_node", visited: false, shortest_path: false };
-//     }
-//     else {
-//         dispatchedMovedStart([grid_size - 2, grid_size - 2])
-//         shallow_copy[grid_size - 2][grid_size - 2] = { type: "start_node", visited: false, shortest_path: false };
-//     }
-//     if (end[0] < grid_size - 1 && end[1] < grid_size - 1) {
-//         shallow_copy[end[0]][end[1]] = { type: "end_node", visited: false, shortest_path: false };
-//     }
-//     else {
-//         dispatchedMovedEnd([grid_size - 1, grid_size - 1])
-//         shallow_copy[grid_size - 1][grid_size - 1] = { type: "end_node", visited: false, shortest_path: false };
-
-//     }
-
-//     if (!isClearAll) {
-//         const weights = store.getState().nodes.weight_coordinates;
-//         const additional_locations = store.getState().nodes.additional_destination_coordinates;
-//         const walls = store.getState().nodes.wall_coordinates;
-
-//         while (Object.keys(additional_locations).length !== 0) {
-//             let keys = Object.keys(additional_locations);
-//             dispatchedRemovedAdditionalDestination(keys[0]);
-//         }
-//         // for (let i = 0; i < additional_locations.length; i++) {
-//         //     let current = additional_locations[i]
-//         //     if (current[0] < grid_size && current[1] < grid_size) {
-//         //         shallow_copy[current[0]][current[1]] = { type: "additional_destination_node", visited: false, shortest_path: false };
-//         //     }
-//         //     else
-//         //     {
-//         //         dispatchedRemovedAdditionalDestination(current);    
-//         //     }
-
-
-//     for (let i = 0; i < weights.length; i++) {
-//         let current = weights[i]
-//         if (current[0] < grid_size && current[1] < grid_size) {
-//             shallow_copy[current[0]][current[1]] = { type: "weight_node", visited: false, shortest_path: false };
-//         }
-//         else {
-//             dispatchedRemovedWeight(current);
-//         }
-//     }
-
-
-
-//     for (let i = 0; i < walls.length; i++) {
-//         let current = walls[i]
-//         if (current[0] < grid_size && current[1] < grid_size) {
-//             shallow_copy[current[0]][current[1]] = { type: "wall_node", visited: false, shortest_path: false };
-//         }
-//         else {
-//             dispatchedRemovedWall(current);
-//         }
-//     }
-
-
-// }
-
-// dispatchedMovedStart()
-// dispatchedClearMatrix(shallow_copy);
-// dispatchedChangedNodeType(matrix)
-// }
-
 export async function clearPath() {
     const start = await store.getState().nodes.start_coordinates;
     const end = await store.getState().nodes.end_coordinates;
@@ -110,7 +34,7 @@ export async function clearAll()
             var type = null;
             if(node['type'] !== "start_node" && node["type"] !== "end_node")
             {
-               type = node['type']g
+               type = node['type']
                node = { type: "empty_node", visited: false, shortest_path: false} 
                copy[i][j] = node;
             }
--- src/react/modules/controls.js
+++ src/react/modules/controls.js
@@ -13,7 +13,7 @@ export default function Controls() {
     return (
         <div className="grid controls">
             <div className={"flex row row-algorithms"} >
-                <MasterCTA type={"select"} id={"algorithms"} className="choicebox" onChange={() => {dispatchedStoppedSearch();await clearPath()}}>
+                <MasterCTA type={"select"} id={"algorithms"} className="choicebox" onChange={async () => {await dispatchedStoppedSearch();await clearPath()}}>
                     <option value="a*">A*</option>
                     <option value="bfs">Breadth First Search</option>
                     <option value="dfs">Depth First Search</option>
@@ -22,7 +22,7 @@ export default function Controls() {
             </div>
 
             <div className={"flex row row-node-types"}>
-                <MasterCTA type={"select"} id={"node-types"} className="choicebox" onChange={() => {dispatchedStoppedSearch(); await clearPath()}}>
+                <MasterCTA type={"select"} id={"node-types"} className="choicebox" onChange={async () => {await dispatchedStoppedSearch(); await clearPath()}}>
                     <option value="additional_destination_node">Add An Additional Destination</option>
                     <option value="empty_node">Remove An Item</option>
                     <option value="end_node">Move The Ending Place</option>
